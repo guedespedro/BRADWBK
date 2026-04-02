788a3bc39211165e357a1c5a9e331b8b05cbb6d6
guedespedro <pedroguazevedo123@gmail.com>
2026-04-01 21:24:49 -0300

    primeiro upload dos arquivos do exercicio 1 e 2

---
+++ ls2/ex1eex2/src/main/java/ifsp/ls2/ex1eex2/Ex1Ex2Application.java
@@ -0,0 +1,11 @@
+package ifsp.ls2.ex1eex2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ex1Ex2Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Ex1Ex2Application.class, args);
+    }
+}
---
+++ ls2/ex1eex2/src/test/java/ifsp/ls2/ex1eex2/Ex1eex2ApplicationTests.java
@@ -0,0 +1,13 @@
+package ifsp.ls2.ex1eex2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ex1eex2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
